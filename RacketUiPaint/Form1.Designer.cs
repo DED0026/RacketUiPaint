6249d629a4d22e9cd85ac61438c3c36c90175add
BuildTools <unconfigured@null.spigotmc.org>
2023-09-22 22:37:56 -0500

    aaaaaaaa work?

--- RacketUiPaint/Form1.Designer.cs
+++ RacketUiPaint/Form1.Designer.cs
@@ -28,13 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.scriptv = new System.Windows.Forms.Button();
+            this.Undo = new System.Windows.Forms.Button();
+            this.scriptthing = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 450);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 410);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // scriptv
+            // 
+            this.scriptv.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.scriptv.Location = new System.Drawing.Point(691, 8);
+            this.scriptv.Name = "scriptv";
+            this.scriptv.Size = new System.Drawing.Size(102, 26);
+            this.scriptv.TabIndex = 1;
+            this.scriptv.Text = "Design View";
+            this.scriptv.UseVisualStyleBackColor = true;
+            this.scriptv.Click += new System.EventHandler(this.scriptv_Click);
+            // 
+            // Undo
+            // 
+            this.Undo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Undo.Location = new System.Drawing.Point(597, 8);
+            this.Undo.Name = "Undo";
+            this.Undo.Size = new System.Drawing.Size(75, 26);
+            this.Undo.TabIndex = 1;
+            this.Undo.Text = "Undo";
+            this.Undo.UseVisualStyleBackColor = true;
+            this.Undo.Click += new System.EventHandler(this.Undo_Click);
+            // 
+            // scriptthing
+            // 
+            this.scriptthing.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.scriptthing.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scriptthing.Location = new System.Drawing.Point(0, 41);
+            this.scriptthing.Name = "scriptthing";
+            this.scriptthing.Size = new System.Drawing.Size(800, 410);
+            this.scriptthing.TabIndex = 2;
+            this.scriptthing.Text = "; welcome to RacketUiPaint\n; click Design View at the top to make something for r" +
+    "acket\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 860);
+            this.ControlBox = false;
+            this.Controls.Add(this.scriptthing);
+            this.Controls.Add(this.Undo);
+            this.Controls.Add(this.scriptv);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
             this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Gray;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button scriptv;
+        private System.Windows.Forms.Button Undo;
+        private System.Windows.Forms.RichTextBox scriptthing;
     }
 }
 
--- RacketUiPaint/Form1.cs
+++ RacketUiPaint/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,85 @@ namespace RacketUiPaint
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                Close();
+            }
+            if (e.KeyCode == Keys.I)
+            {
+                MessageBox.Show("thanks for using racketuipaint\rthis application is made to paint in racket using shapes\rpress:\rESC to close the app\rI to close this menu\rC for colors\rS for shapes");
+            }
+        }
+
+        private bool isDrawing = false;
+        private Point previousPoint;
+        private Pen drawingPen = new Pen(Color.HotPink, 1); // Customize the pen as needed
+        private List<GraphicsPath> drawnPaths = new List<GraphicsPath>();
+        private int currentPathIndex = -1; // Current index in the list
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDrawing = true;
+            previousPoint = e.Location;
+            // Create a new GraphicsPath for the current drawing
+            GraphicsPath path = new GraphicsPath();
+            //path.AddLine(previousPoint, previousPoint);
+            drawnPaths.Add(path);
+            currentPathIndex++;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                using (Graphics g = pictureBox1.CreateGraphics())
+                {
+                    g.DrawLine(drawingPen, previousPoint, e.Location);
+                    previousPoint = e.Location;
+                    drawnPaths[currentPathIndex].AddLine(previousPoint, e.Location);
+                }
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDrawing = false;
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (var path in drawnPaths)
+            {
+                e.Graphics.DrawPath(drawingPen, path);
+            }
+        }
+
+        private void scriptv_Click(object sender, EventArgs e)
+        {
+            if (scriptthing.Visible)
+            {
+                scriptthing.Visible = false;
+                scriptv.Text = "Script View";
+            }
+            else
+            {
+                scriptthing.Visible = true;
+                scriptv.Text = "Design View";
+            }
+        }
+
+        private void Undo_Click(object sender, EventArgs e)
+        {
+            if (currentPathIndex >= 0)
+            {
+                drawnPaths.RemoveAt(currentPathIndex);
+                currentPathIndex--;
+                pictureBox1.Refresh(); // Repaint the PictureBox
+            }
         }
     }
 }

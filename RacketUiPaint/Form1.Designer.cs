01834f2be8ef301f8995754a19626b3aabfbdc67
BuildTools <unconfigured@null.spigotmc.org>
2023-09-28 13:42:35 -0500

    many progress

--- RacketUiPaint/Form1.Designer.cs
+++ RacketUiPaint/Form1.Designer.cs
@@ -31,77 +31,132 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
             this.scriptv = new System.Windows.Forms.Button();
-            this.Undo = new System.Windows.Forms.Button();
             this.scriptthing = new System.Windows.Forms.RichTextBox();
+            this.color = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.ModeButton = new System.Windows.Forms.Button();
+            this.DrawMode = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 450);
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 26);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 0, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(800, 410);
+            this.pictureBox1.Size = new System.Drawing.Size(800, 428);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
-            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
-            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
-            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
-            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // scriptv
             // 
+            this.scriptv.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.scriptv.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.scriptv.Location = new System.Drawing.Point(691, 8);
+            this.scriptv.Location = new System.Drawing.Point(699, 0);
+            this.scriptv.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.scriptv.Name = "scriptv";
-            this.scriptv.Size = new System.Drawing.Size(102, 26);
+            this.scriptv.Size = new System.Drawing.Size(101, 26);
             this.scriptv.TabIndex = 1;
             this.scriptv.Text = "Design View";
             this.scriptv.UseVisualStyleBackColor = true;
             this.scriptv.Click += new System.EventHandler(this.scriptv_Click);
             // 
-            // Undo
-            // 
-            this.Undo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Undo.Location = new System.Drawing.Point(597, 8);
-            this.Undo.Name = "Undo";
-            this.Undo.Size = new System.Drawing.Size(75, 26);
-            this.Undo.TabIndex = 1;
-            this.Undo.Text = "Undo";
-            this.Undo.UseVisualStyleBackColor = true;
-            this.Undo.Click += new System.EventHandler(this.Undo_Click);
-            // 
             // scriptthing
             // 
+            this.scriptthing.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.scriptthing.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.scriptthing.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.scriptthing.Location = new System.Drawing.Point(0, 41);
+            this.scriptthing.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scriptthing.Location = new System.Drawing.Point(0, 29);
+            this.scriptthing.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.scriptthing.Name = "scriptthing";
-            this.scriptthing.Size = new System.Drawing.Size(800, 410);
+            this.scriptthing.Size = new System.Drawing.Size(800, 425);
             this.scriptthing.TabIndex = 2;
             this.scriptthing.Text = "; welcome to RacketUiPaint\n; click Design View at the top to make something for r" +
-    "acket\n";
+    "acket\n(define base(rectangle 0 0 \"solid\" \"white\"))";
+            // 
+            // color
+            // 
+            this.color.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.color.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.color.Location = new System.Drawing.Point(571, 0);
+            this.color.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.color.Name = "color";
+            this.color.Size = new System.Drawing.Size(65, 26);
+            this.color.TabIndex = 1;
+            this.color.Text = "Color";
+            this.color.UseVisualStyleBackColor = true;
+            this.color.Click += new System.EventHandler(this.color_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Clear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Clear.Location = new System.Drawing.Point(635, 0);
+            this.Clear.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(65, 26);
+            this.Clear.TabIndex = 1;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // ModeButton
+            // 
+            this.ModeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ModeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ModeButton.Location = new System.Drawing.Point(469, 0);
+            this.ModeButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.ModeButton.Name = "ModeButton";
+            this.ModeButton.Size = new System.Drawing.Size(103, 26);
+            this.ModeButton.TabIndex = 1;
+            this.ModeButton.Text = "Mode: outline";
+            this.ModeButton.UseVisualStyleBackColor = true;
+            this.ModeButton.Click += new System.EventHandler(this.ModeButton_Click);
+            // 
+            // DrawMode
+            // 
+            this.DrawMode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.DrawMode.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DrawMode.FormattingEnabled = true;
+            this.DrawMode.Items.AddRange(new object[] {
+            "Rectangle",
+            "Mouse (Connected)",
+            "Mouse (Seperate)",
+            "Circle",
+            "Triangle"});
+            this.DrawMode.Location = new System.Drawing.Point(0, 0);
+            this.DrawMode.Name = "DrawMode";
+            this.DrawMode.Size = new System.Drawing.Size(144, 24);
+            this.DrawMode.TabIndex = 1;
+            this.DrawMode.SelectedIndexChanged += new System.EventHandler(this.DrawMode_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(800, 860);
+            this.ClientSize = new System.Drawing.Size(800, 454);
             this.ControlBox = false;
+            this.Controls.Add(this.DrawMode);
             this.Controls.Add(this.scriptthing);
-            this.Controls.Add(this.Undo);
+            this.Controls.Add(this.color);
             this.Controls.Add(this.scriptv);
             this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ModeButton);
+            this.Controls.Add(this.Clear);
             this.DoubleBuffered = true;
             this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.ShowIcon = false;
-            this.Text = "Form1";
-            this.TransparencyKey = System.Drawing.Color.Gray;
-            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.Text = "RacketUiPaint";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -112,8 +167,11 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.ColorDialog colorDialog1;
         private System.Windows.Forms.Button scriptv;
-        private System.Windows.Forms.Button Undo;
         private System.Windows.Forms.RichTextBox scriptthing;
+        private System.Windows.Forms.Button color;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button ModeButton;
+        private System.Windows.Forms.ComboBox DrawMode;
     }
 }
 
--- RacketUiPaint/Form1.cs
+++ RacketUiPaint/Form1.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Configuration;
 using System.Drawing.Drawing2D;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,65 +16,19 @@ namespace RacketUiPaint
 {
     public partial class Form1 : Form
     {
+        string rectangleformula = "(define newran(overlay/xy (rectangle width height \"mode\" color) x y oldran))";
+        string lineformula = "(define newran (add-line oldran point1 point2 color))";
+        string triangleformula = "(define newran (add-polygon oldran (list (make-posn point1) (make-posn point2) (make-posn point3)) \"mode\" color))";
+        string oldrandomstring = "base";
         public Form1()
         {
             InitializeComponent();
+            DrawMode.SelectedIndex = 0;
         }
+        string[] alphabet = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
+        static Color colorthingy = Color.Black;
 
-        private void Form1_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Escape)
-            {
-                Close();
-            }
-            if (e.KeyCode == Keys.I)
-            {
-                MessageBox.Show("thanks for using racketuipaint\rthis application is made to paint in racket using shapes\rpress:\rESC to close the app\rI to close this menu\rC for colors\rS for shapes");
-            }
-        }
-
-        private bool isDrawing = false;
-        private Point previousPoint;
-        private Pen drawingPen = new Pen(Color.HotPink, 1); // Customize the pen as needed
-        private List<GraphicsPath> drawnPaths = new List<GraphicsPath>();
-        private int currentPathIndex = -1; // Current index in the list
-
-        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
-        {
-            isDrawing = true;
-            previousPoint = e.Location;
-            // Create a new GraphicsPath for the current drawing
-            GraphicsPath path = new GraphicsPath();
-            //path.AddLine(previousPoint, previousPoint);
-            drawnPaths.Add(path);
-            currentPathIndex++;
-        }
-
-        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (isDrawing)
-            {
-                using (Graphics g = pictureBox1.CreateGraphics())
-                {
-                    g.DrawLine(drawingPen, previousPoint, e.Location);
-                    previousPoint = e.Location;
-                    drawnPaths[currentPathIndex].AddLine(previousPoint, e.Location);
-                }
-            }
-        }
-
-        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
-        {
-            isDrawing = false;
-        }
-
-        private void pictureBox1_Paint(object sender, PaintEventArgs e)
-        {
-            foreach (var path in drawnPaths)
-            {
-                e.Graphics.DrawPath(drawingPen, path);
-            }
-        }
+        string racketcolor = "0 0 0";
 
         private void scriptv_Click(object sender, EventArgs e)
         {
@@ -87,14 +44,221 @@ namespace RacketUiPaint
             }
         }
 
-        private void Undo_Click(object sender, EventArgs e)
+        int turn = 1;
+
+        Point point1;
+        Point point2;
+
+        private async void drawrectangle()
         {
-            if (currentPathIndex >= 0)
+            switch (turn)
             {
-                drawnPaths.RemoveAt(currentPathIndex);
-                currentPathIndex--;
-                pictureBox1.Refresh(); // Repaint the PictureBox
+                case 1:
+                    point1 = pictureBox1.PointToClient(Cursor.Position);
+                    turn++;
+                    break;
+                case 2:
+                    Pen drawingPen = new Pen(colorthingy, 1); 
+                    point2 = pictureBox1.PointToClient(Cursor.Position);
+                    int width = Math.Abs(point2.X - point1.X); 
+                    int height = Math.Abs(point2.Y - point1.Y); 
+
+                    int x = Math.Min(point1.X, point2.X);
+                    int y = Math.Min(point1.Y, point2.Y); 
+                    if (mode == "outline")
+                        pictureBox1.CreateGraphics().DrawRectangle(drawingPen, new Rectangle(x, y, width, height));
+                    else
+                        pictureBox1.CreateGraphics().FillRectangle(new SolidBrush(drawingPen.Color), new Rectangle(x, y, width, height));
+
+                    drawingPen.Dispose();
+
+                    string newrandomstring = "";
+                loob:
+                    while (5 > newrandomstring.Length)
+                    {
+                        newrandomstring += alphabet[new Random().Next(0, 62)];
+                        await Task.Delay(1);
+                    }
+                    if (scriptthing.Text.Contains(newrandomstring))
+                    {
+                        await Task.Delay(200);
+                        goto loob;
+                    }
+                    x = x - x * 2;
+                    y = y - y * 2;
+                    scriptthing.Text += "\r" + rectangleformula.Replace("newran", newrandomstring).Replace("height", height.ToString()).Replace("width", width.ToString()).Replace(" x ", " " + x.ToString() + " ").Replace(" y ", " " + y.ToString() + " ").Replace("color", "(make-color " + racketcolor + ")").Replace("mode", mode).Replace("oldran", oldrandomstring);
+                    oldrandomstring = newrandomstring;
+                    turn = 1;
+                    break;
             }
+        }
+
+
+        private async void drawlineconnected()
+        {
+            switch (turn)
+            {
+                case 1:
+                    point1 = pictureBox1.PointToClient(Cursor.Position);
+                    turn++;
+                    break;
+                case 2:
+                    Pen drawingPen = new Pen(colorthingy, 1); 
+                    point2 = pictureBox1.PointToClient(Cursor.Position);
+                    pictureBox1.CreateGraphics().DrawLine(drawingPen, point1, point2);
+                    drawingPen.Dispose();
+
+                    string newrandomstring = "";
+                loob:
+                    while (5 > newrandomstring.Length)
+                    {
+                        newrandomstring += alphabet[new Random().Next(0, 62)];
+                        await Task.Delay(1);
+                    }
+                    if (scriptthing.Text.Contains(newrandomstring))
+                    {
+                        await Task.Delay(200);
+                        goto loob;
+                    }
+                    scriptthing.Text += "\r" + lineformula.Replace("newran", newrandomstring).Replace("point1", point1.X + " " + point1.Y).Replace("point2", point2.X + " " + point2.Y).Replace("color", "(make-color " + racketcolor + ")").Replace("oldran", oldrandomstring);
+                    oldrandomstring = newrandomstring;
+                    point1 = point2;
+                    break;
+            }
+        }
+
+        private async void drawlineseperate()
+        {
+            switch (turn)
+            {
+                case 1:
+                    point1 = pictureBox1.PointToClient(Cursor.Position);
+                    turn++;
+                    break;
+                case 2:
+                    Pen drawingPen = new Pen(colorthingy, 1); // Customize the pen as needed
+                    point2 = pictureBox1.PointToClient(Cursor.Position);
+                    pictureBox1.CreateGraphics().DrawLine(drawingPen, point1, point2);
+                    drawingPen.Dispose();
+
+                    string newrandomstring = "";
+                loob:
+                    while (5 > newrandomstring.Length)
+                    {
+                        newrandomstring += alphabet[new Random().Next(0, 62)];
+                        await Task.Delay(1);
+                    }
+                    if (scriptthing.Text.Contains(newrandomstring))
+                    {
+                        await Task.Delay(200);
+                        goto loob;
+                    }
+                    scriptthing.Text += "\r" + lineformula.Replace("newran", newrandomstring).Replace("point1", point1.X + " " + point1.Y).Replace("point2", point2.X + " " + point2.Y).Replace("color", "(make-color " + racketcolor + ")").Replace("oldran", oldrandomstring);
+                    oldrandomstring = newrandomstring;
+                    turn = 1;
+                    break;
+            }
+        }
+
+        private async void drawtriangle()
+        {
+            switch (turn)
+            {
+                case 1:
+                    point1 = pictureBox1.PointToClient(Cursor.Position);
+                    turn++;
+                    break;
+                case 2:
+                    point2 = pictureBox1.PointToClient(Cursor.Position);
+                    turn++;
+                    break; 
+                case 3:
+                    Point point3 = pictureBox1.PointToClient(Cursor.Position);
+                    Pen drawingPen = new Pen(colorthingy, 1); 
+                    int width = Math.Abs(point2.X - point1.X); 
+                    int height = Math.Abs(point2.Y - point1.Y); 
+
+                    int x = Math.Min(point1.X, point2.X); 
+                    int y = Math.Min(point1.Y, point2.Y);
+                    Point[] points = {point1, point2, point3};
+                    if (mode == "outline")
+                        pictureBox1.CreateGraphics().DrawPolygon(drawingPen, points);
+                    else
+                        pictureBox1.CreateGraphics().FillPolygon(new SolidBrush(drawingPen.Color), points);
+                    drawingPen.Dispose();
+
+                    string newrandomstring = "";
+                loob:
+                    while (5 > newrandomstring.Length)
+                    {
+                        newrandomstring += alphabet[new Random().Next(0, 62)];
+                        await Task.Delay(1);
+                    }
+                    if (scriptthing.Text.Contains(newrandomstring))
+                    {
+                        await Task.Delay(200);
+                        goto loob;
+                    }
+                    x = x - x * 2;
+                    y = y - y * 2;
+                    //(define newran (add-polygon oldran (list (make-posn point1) (make-posn point2) (make-posn point3) \"mode\" color))
+                    scriptthing.Text += "\r" + triangleformula.Replace("newran", newrandomstring).Replace("point1", point1.X + " " + point1.Y).Replace("point2", point2.X + " " + point2.Y).Replace("point3", point3.X + " " + point3.Y).Replace("color", "(make-color " + racketcolor + ")").Replace("mode", mode).Replace("oldran", oldrandomstring);
+                    oldrandomstring = newrandomstring;
+                    turn = 1;
+                    break;
+            }
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            switch (DrawMode.SelectedIndex)
+            {
+                case 0:
+                    drawrectangle();
+                    break;
+                case 1:
+                    drawlineconnected();
+                    break;
+                case 2:
+                    drawlineseperate();
+                    break;
+                case 4:
+                    drawtriangle();
+                    break;
+                default:
+                    MessageBox.Show("Uncoded Code Reached: " + DrawMode.SelectedIndex);
+                    break;
+            }
+        }
+
+        private void color_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            colorthingy = colorDialog1.Color;
+            racketcolor = colorthingy.R + " " + colorthingy.G + " " + colorthingy.B;
+            color.ForeColor = colorthingy;
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Invalidate();
+            oldrandomstring = "base";
+            scriptthing.Text = "; welcome to RacketUiPaint\r; click Design View at the top to make something for racket\r(define base(rectangle 0 0 \"solid\" \"white\"))";
+        }
+        string mode = "outline";
+        private void ModeButton_Click(object sender, EventArgs e)
+        {
+            if (mode == "outline")
+                mode = "solid";
+            else
+                mode = "outline";
+            ModeButton.Text = "Mode: " + mode;
+        }
+
+        private void DrawMode_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            turn = 1;
         }
     }
 }

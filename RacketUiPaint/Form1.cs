3ea4700900fe72da3811ed87df05b61f3298bf1a
BuildTools <unconfigured@null.spigotmc.org>
2023-10-17 22:22:48 -0500

    speed some stuff up move variables to the top move drawing stuff up move non drawing stuff down remove some spaces modify some notes shorten/simplify code

--- RacketUiPaint/Form1.cs
+++ RacketUiPaint/Form1.cs
@@ -15,9 +15,11 @@ namespace RacketUiPaint
         Point point2;
         readonly string[] alphabet = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
         static Color colorthingy = System.Drawing.Color.Black;
+        Pen drawingPen = new Pen(colorthingy, 1);
         string pointsforracket;
         string racketcolor = "0 0 0";
         string oldrandomstring = "base";
+        string mode = "outline";
         string newrandomstring;
         readonly string rectangleformula = "(define newran(overlay/xy (rectangle width height \"mode\" color) x y oldran))";
         readonly string lineformula = "(define newran(add-line oldran point1 point2 color))";
@@ -30,6 +32,7 @@ namespace RacketUiPaint
         readonly Stack<string> randomstack = new Stack<string>();
         readonly Stack<string> scriptstack = new Stack<string>();
         readonly List<Point> pointslist = new List<Point>();
+
         public Form1()
         {
             InitializeComponent();
@@ -44,20 +47,6 @@ namespace RacketUiPaint
             Drawbox.AllowDrop = true;
         }
 
-        private void Scripttoggle_Click(object sender, EventArgs e)
-        {
-            if (Scriptbox.Visible)
-            {
-                Scriptbox.Visible = false;
-                Scripttoggle.Text = "Script View";
-            }
-            else
-            {
-                Scriptbox.Visible = true;
-                Scripttoggle.Text = "Design View";
-            }
-        }
-
         private async void DrawRectangle()
         {
             switch (turn)
@@ -67,7 +56,6 @@ namespace RacketUiPaint
                     turn++;
                     break;
                 case 2:
-                    Pen drawingPen = new Pen(colorthingy, 1);
                     point2 = Drawbox.PointToClient(Cursor.Position);
                     int width = Math.Abs(point2.X - point1.X);
                     int height = Math.Abs(point2.Y - point1.Y);
@@ -79,7 +67,6 @@ namespace RacketUiPaint
                     else
                         Graphics.FromImage(drawingBitmap).FillRectangle(drawingPen.Brush, new Rectangle(x, y, width, height));
                     Drawbox.Image = drawingBitmap;
-                    drawingPen.Dispose();
                 loob:
                     newrandomstring = "";
                     while (5 > newrandomstring.Length)
@@ -115,7 +102,6 @@ namespace RacketUiPaint
                     turn++;
                     break;
                 case 3:
-                    Pen drawingPen = new Pen(colorthingy, 1);
                     Point point3 = Drawbox.PointToClient(Cursor.Position);
                     int radius = Convert.ToInt32(CalcDistance(point1, point2)) / 2;
                     int x = point3.X - radius;
@@ -125,7 +111,6 @@ namespace RacketUiPaint
                     else
                         Graphics.FromImage(drawingBitmap).FillEllipse(new SolidBrush(drawingPen.Color), point3.X - radius, point3.Y - radius, 2 * radius, 2 * radius);
                     Drawbox.Image = drawingBitmap;
-                    drawingPen.Dispose();
                 loob:
                     newrandomstring = "";
                     while (5 > newrandomstring.Length)
@@ -148,7 +133,6 @@ namespace RacketUiPaint
             }
         }
 
-
         private async void DrawLineConnected()
         {
             switch (turn)
@@ -158,10 +142,8 @@ namespace RacketUiPaint
                     turn++;
                     break;
                 case 2:
-                    Pen drawingPen = new Pen(colorthingy, 1);
                     point2 = Drawbox.PointToClient(Cursor.Position);
                     Graphics.FromImage(drawingBitmap).DrawLine(drawingPen, point1, point2);
-                    drawingPen.Dispose();
                     Drawbox.Image = drawingBitmap;
                 loob:
                     newrandomstring = "";
@@ -192,10 +174,8 @@ namespace RacketUiPaint
                     turn++;
                     break;
                 case 2:
-                    Pen drawingPen = new Pen(colorthingy, 1); // Customize the pen as needed
                     point2 = Drawbox.PointToClient(Cursor.Position);
                     Graphics.FromImage(drawingBitmap).DrawLine(drawingPen, point1, point2);
-                    drawingPen.Dispose();
                     Drawbox.Image = drawingBitmap;
                 loob:
                     newrandomstring = "";
@@ -231,13 +211,11 @@ namespace RacketUiPaint
                     break;
                 case 3:
                     Point point3 = Drawbox.PointToClient(Cursor.Position);
-                    Pen drawingPen = new Pen(colorthingy, 1);
                     Point[] points = { point1, point2, point3 };
                     if (mode == "outline")
                         Graphics.FromImage(drawingBitmap).DrawPolygon(drawingPen, points);
                     else
                         Graphics.FromImage(drawingBitmap).FillPolygon(drawingPen.Brush, points);
-                    drawingPen.Dispose();
                     Drawbox.Image = drawingBitmap;
                 loob:
                     newrandomstring = "";
@@ -268,15 +246,47 @@ namespace RacketUiPaint
                 PolyClose.Show();
         }
 
+        private async void PolyClose_Click(object sender, EventArgs e)
+        {
+            if (turn > 3)
+            {
+                if (mode == "outline")
+                    Graphics.FromImage(drawingBitmap).DrawPolygon(new Pen(colorthingy, 1), pointslist.ToArray());
+                else
+                    Graphics.FromImage(drawingBitmap).FillPolygon(new SolidBrush(colorthingy), pointslist.ToArray());
+                Drawbox.Image = drawingBitmap;
+            loob:
+                newrandomstring = "";
+                while (5 > newrandomstring.Length)
+                {
+                    newrandomstring += alphabet[new Random().Next(0, 62)];
+                    await Task.Delay(1);
+                }
+                if (Scriptbox.Text.Contains(newrandomstring))
+                {
+                    await Task.Delay(200);
+                    goto loob;
+                }
+                pointsforracket = "";
+                foreach (Point point in pointslist)
+                    pointsforracket += pointformula.Replace(" x", " " + point.X).Replace(" y", " " + point.Y);
+                //(define newran (add-polygon oldran (listpoints \"mode\" color))
+                Scriptbox.Text += "\r" + polygonformula.Replace("newran", newrandomstring).Replace("points", pointsforracket).Replace("mode", mode).Replace("color", "(make-color " + racketcolor + ")").Replace("oldran", oldrandomstring);
+                oldrandomstring = newrandomstring;
+                turn = 1;
+                pointslist.Clear();
+                SaveState();
+            }
+            PolyClose.Hide();
+        }
+
         private async void DrawText()
         {
             if (Number_Box.Text.Length > 0 && int.Parse(Number_Box.Text) > 0)
             {
                 Point pointthingy = Drawbox.PointToClient(Cursor.Position);
                 Graphics.FromImage(drawingBitmap).DrawString(Text_box.Text, new Font("Arial", int.Parse(Number_Box.Text)), new Pen(colorthingy).Brush, pointthingy);
-                
                 //(define newran(overlay/xy (text \"string\" size color) x y oldran))
-
                 pointthingy.X = -pointthingy.X;
                 pointthingy.Y = -pointthingy.Y;
             loob:
@@ -336,6 +346,7 @@ namespace RacketUiPaint
             colorthingy = Colorpicker.Color;
             racketcolor = colorthingy.R + " " + colorthingy.G + " " + colorthingy.B;
             Color.ForeColor = colorthingy;
+            drawingPen = new Pen(colorthingy, 1);
         }
 
         private void Clear_Click(object sender, EventArgs e)
@@ -358,7 +369,21 @@ namespace RacketUiPaint
             Drawbox.Invalidate();
             SaveState();
         }
-        string mode = "outline";
+
+        private void Scripttoggle_Click(object sender, EventArgs e)
+        {
+            if (Scriptbox.Visible)
+            {
+                Scriptbox.Visible = false;
+                Scripttoggle.Text = "Script View";
+            }
+            else
+            {
+                Scriptbox.Visible = true;
+                Scripttoggle.Text = "Design View";
+            }
+        }
+
         private void ModeButton_Click(object sender, EventArgs e)
         {
             if (mode == "outline")
@@ -391,8 +416,9 @@ namespace RacketUiPaint
                 thingy.Dispose();
             }
         }
-        double CalcDistance(Point p1, Point p2) => Math.Sqrt((p1.X - p2.X) * (p1.X - p2.X) + (p1.Y - p2.Y) * (p1.Y - p2.Y));// < d * d
 
+        double CalcDistance(Point p1, Point p2) => Math.Sqrt((p1.X - p2.X) * (p1.X - p2.X) + (p1.Y - p2.Y) * (p1.Y - p2.Y));
+        
         bool timeout = false;
         private async void Drawbox_MouseMove(object sender, MouseEventArgs e)
         {
@@ -402,69 +428,56 @@ namespace RacketUiPaint
             }
             else return;
             //preview code, runs every time the mouse moves on the Drawbox.
+            Drawbox.Invalidate();
+            await Task.Delay(1);
             switch (DrawMode.SelectedIndex)
             {
                 case 0:
                     //rectangle
                     if (turn == 2)
                     {
-                        Pen drawingPen = new Pen(colorthingy, 1);
                         point2 = Drawbox.PointToClient(Cursor.Position);
                         int width = Math.Abs(point2.X - point1.X);
                         int height = Math.Abs(point2.Y - point1.Y);
                         int x = Math.Min(point1.X, point2.X);
                         int y = Math.Min(point1.Y, point2.Y);
-                        Drawbox.Invalidate();
-                        await Task.Delay(5);
                         if (mode == "outline")
                             Drawbox.CreateGraphics().DrawRectangle(drawingPen, new Rectangle(x, y, width, height));
                         else
                             Drawbox.CreateGraphics().FillRectangle(drawingPen.Brush, new Rectangle(x, y, width, height));
-                        drawingPen.Dispose();
                     }
                     break;
                 case 1:
                 //connectedline
+                //goes through to seperate line
                 case 2:
                     //seperate line
                     if (turn == 2)
                     {
-                        Pen drawingPen = new Pen(colorthingy, 1); // Customize the pen as needed
                         point2 = Drawbox.PointToClient(Cursor.Position);
-                        Drawbox.Invalidate();
-                        await Task.Delay(5);
                         Drawbox.CreateGraphics().DrawLine(drawingPen, point1, point2);
-                        drawingPen.Dispose();
                     }
                     break;
                 case 3:
                     //circle
                     if (turn == 2)
                     {
-                        Pen drawingPen = new Pen(colorthingy, 1);
-                        Point point2 = Drawbox.PointToClient(Cursor.Position);
+                        point2 = Drawbox.PointToClient(Cursor.Position);
                         int radius = Convert.ToInt32(CalcDistance(point1, point2)) / 2;
-                        Drawbox.Invalidate();
-                        await Task.Delay(5);
                         Point center = new Point((point1.X + point2.X) / 2, (point1.Y + point2.Y) / 2);
                         if (mode == "outline")
                             Drawbox.CreateGraphics().DrawEllipse(drawingPen, center.X - radius, center.Y - radius, 2 * radius, 2 * radius);//DrawRectangle(drawingPen, new Rectangle(x, y, width, height));
                         else
                             Drawbox.CreateGraphics().FillEllipse(drawingPen.Brush, center.X - radius, center.Y - radius, 2 * radius, 2 * radius);
-                        drawingPen.Dispose();
                     }
                     if (turn == 3)
                     {
-                        Pen drawingPen = new Pen(colorthingy, 1);
                         Point point3 = Drawbox.PointToClient(Cursor.Position);
                         int radius = Convert.ToInt32(CalcDistance(point1, point2)) / 2;
-                        Drawbox.Invalidate();
-                        await Task.Delay(5);
                         if (mode == "outline")
                             Drawbox.CreateGraphics().DrawEllipse(drawingPen, point3.X - radius, point3.Y - radius, 2 * radius, 2 * radius);//DrawRectangle(drawingPen, new Rectangle(x, y, width, height));
                         else
                             Drawbox.CreateGraphics().FillEllipse(drawingPen.Brush, point3.X - radius, point3.Y - radius, 2 * radius, 2 * radius);
-                        drawingPen.Dispose();
                     }
                     break;
                 case 4:
@@ -472,23 +485,17 @@ namespace RacketUiPaint
                     if (turn == 3)
                     {
                         Point point3 = Drawbox.PointToClient(Cursor.Position);
-                        Pen drawingPen = new Pen(colorthingy, 1);
                         Point[] points = { point1, point2, point3 };
-                        Drawbox.Invalidate();
-                        await Task.Delay(5);
                         if (mode == "outline")
                             Drawbox.CreateGraphics().DrawPolygon(drawingPen, points);
                         else
                             Drawbox.CreateGraphics().FillPolygon(drawingPen.Brush, points);
-                        drawingPen.Dispose();
                     }
                     break;
                 case 5:
                     //    //polygons
                     if (turn > 2)
                     {
-                        Drawbox.Invalidate();
-                        await Task.Delay(5);
                         List<Point> points = new List<Point>(pointslist)
                         {
                             Drawbox.PointToClient(Cursor.Position)
@@ -500,12 +507,8 @@ namespace RacketUiPaint
                     }
                     break;
                 case 6:
-
-                    Drawbox.Invalidate();
-                    await Task.Delay(5);
                     if (Number_Box.Text.Length > 0 && int.Parse(Number_Box.Text) > 0)
                     {
-                        await Task.Delay(5);
                         Drawbox.CreateGraphics().DrawString(Text_box.Text, new Font("Arial", int.Parse(Number_Box.Text)), new Pen(colorthingy).Brush, Drawbox.PointToClient(Cursor.Position));
                     }
                     break;
@@ -514,44 +517,8 @@ namespace RacketUiPaint
             timeout = false;
         }
 
-        private async void PolyClose_Click(object sender, EventArgs e)
-        {
-            if (turn > 3)
-            {
-                if (mode == "outline")
-                    Graphics.FromImage(drawingBitmap).DrawPolygon(new Pen(colorthingy, 1), pointslist.ToArray());
-                else
-                    Graphics.FromImage(drawingBitmap).FillPolygon(new SolidBrush(colorthingy), pointslist.ToArray());
-                Drawbox.Image = drawingBitmap;
-
-                
-            loob:
-                newrandomstring = "";
-                while (5 > newrandomstring.Length)
-                {
-                    newrandomstring += alphabet[new Random().Next(0, 62)];
-                    await Task.Delay(1);
-                }
-                if (Scriptbox.Text.Contains(newrandomstring))
-                {
-                    await Task.Delay(200);
-                    goto loob;
-                }
-                pointsforracket = "";
-                foreach(Point point in pointslist)
-                    pointsforracket += pointformula.Replace(" x", " " + point.X).Replace(" y", " " + point.Y);
-                //(define newran (add-polygon oldran (listpoints \"mode\" color))
-                Scriptbox.Text += "\r" + polygonformula.Replace("newran", newrandomstring).Replace("points", pointsforracket).Replace("mode", mode).Replace("color", "(make-color " + racketcolor + ")").Replace("oldran", oldrandomstring);
-                oldrandomstring = newrandomstring;
-                turn = 1;
-                SaveState();
-            }
-            PolyClose.Hide();
-        }
-
         private void Number_Box_KeyPress(object sender, KeyPressEventArgs e)
         {
-            // Allow only numeric characters, Backspace, Delete, and arrow keys
             if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
             {
                 e.Handled = true;
@@ -567,19 +534,11 @@ namespace RacketUiPaint
                 scriptstack.Pop();
                 oldrandomstring = randomstack.Peek();
                 Scriptbox.Text = scriptstack.Peek();
-                //pictureBox1.Image = new Bitmap(bitmaphistory.Peek());
                 Graphics.FromImage(drawingBitmap).Clear(System.Drawing.Color.Transparent);
                 Graphics.FromImage(drawingBitmap).DrawImage(bitmaphistory.Peek(), Point.Empty);
                 Drawbox.Image = drawingBitmap;
                 Drawbox.Invalidate();
             }
-
-            //Graphics.FromImage(drawingBitmap).Clear(BackColor);
-            //Graphics.FromImage(drawingBitmap).DrawImage(onebackdrawmap,Point.Empty);
-            //oldrandomstring = onebackoldran;
-            //scriptthing.Text = onebackscript;
-            //pictureBox1.Image = drawingBitmap;
-            //Undoer.Hide();
         }
 
         private void SaveState()
@@ -588,11 +547,6 @@ namespace RacketUiPaint
             bitmaphistory.Push(new Bitmap(Drawbox.Image));
             randomstack.Push(oldrandomstring);
             scriptstack.Push(Scriptbox.Text);
-            //onebackdrawmap = new Bitmap(bitmapthing.Width, bitmapthing.Height);
-            //Graphics.FromImage(onebackdrawmap).DrawImage(pictureBox1.Image, Point.Empty);
-            //onebackoldran = oldrandomstring;
-            //onebackscript = scriptthing.Text;
-            //Undoer.Show();
         }
 
         private void Drawbox_DragEnter(object sender, DragEventArgs e)
